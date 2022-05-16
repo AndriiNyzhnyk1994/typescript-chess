3715043cdcc3aa4bea269b0537d62fa2d5af75b4
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2022-05-16 14:47:15 +0300

    Initial Commit. 1. Created Board & Cell classes and logic to render them. 2. Created: Colors, Player, Figure empty classes yet. 3.  Created BoardComponents & CellComponents (react components) and style to them in App.css

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+const BoardComponent = () => {
+    return (
+        <div className="board">
+            
+        </div>
+    )
+}
+
+
+export default BoardComponent
